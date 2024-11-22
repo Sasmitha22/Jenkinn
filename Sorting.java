31a6db6347a4c11ca8a1b035c3d4e3b55eea4dee
Sasmitha Baskaran <110805208+Sasmitha22@users.noreply.github.com>
2024-11-22 12:30:54 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class Sorting {
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Custom input array
+        int[] arr = { 13, 7, 6, 45, 21, 9, 101,200 };
+
+        // Calling the sort() method present
+        // inside Arrays class
+        Arrays.sort(arr);
+
+        // Printing and display sorted array
+        System.out.printf("Modified arr : %s",
+                          Arrays.toString(arr));
+    }
+}
